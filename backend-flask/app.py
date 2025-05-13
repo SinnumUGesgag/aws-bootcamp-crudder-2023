263ae204580652ae61e8bb89b3f197e535626f1f
SinnumUGesgag A <michael.shrader@icloud.com>
2025-05-13 14:07:21 +0000

    Added Watchtower

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -28,7 +28,21 @@ from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 # <---
 
+# For Cloudwatch Logs --->
+import watchtower
+import	logging
+from time import	strftime
+# <---
 
+# Configuring Logger to use Cloudwatch --->
+LOGGER = logging.getLogger(__name__)
+LOGGER.setlevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("test log")
+# <---
 
 # Honeycomb, Tracing ------->
 # Initializes tracing and an exporter that sends data to Honeycomb
@@ -61,6 +75,7 @@ XRayMiddleware(app, xray_recorder)
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
+
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
@@ -68,6 +83,16 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+# When an Error occurs, this will create a Report/Log -->
+@app.after_request
+@xray_recorder.capture('Error Testing')
+def after_request( response ):
+	timestamp = strftime('[%Y-%b-%d %H:%M]')
+	LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+	return response
+# <---
+
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
