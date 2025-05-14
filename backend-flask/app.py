a47012c2f0e468fc2fe583b0d0c2f811daf55611
SinnumUGesgag A <michael.shrader@icloud.com>
2025-05-14 16:06:31 +0000

    I had put a pause on troubleshooting Watchtower with cloudwatch; it was giving me errors relating to requiring a REGION to be specified; I am not uncommenting those lines of code, and will troubleshoot them further.

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -42,13 +42,13 @@ from flask import got_request_exception
 # <---
 
 # Configuring Logger to use Cloudwatch --->
-#LOGGER = logging.getLogger(__name__)
-#LOGGER.setLevel(logging.DEBUG)
-#console_handler = logging.StreamHandler()
-#cw_handler = watchtower.CloudWatchLogHandler(log_group='Cruddur')
-#LOGGER.addHandler(console_handler)
-#LOGGER.addHandler(cw_handler)
-#LOGGER.info("test log")
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='Cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("test log")
 # <---
 
 # Honeycomb, Tracing ------->
@@ -112,12 +112,12 @@ with app.app_context():
 
 
 # When an Error occurs, this will create a Report/Log for Cloudwatch-->
-#@app.after_request
-#@xray_recorder.capture('Error Testing')
-#def after_request( response ):
-#	timestamp = strftime('[%Y-%b-%d %H:%M]')
-#	LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
-#	return response
+@app.after_request
+@xray_recorder.capture('Error Testing')
+def after_request( response ):
+	timestamp = strftime('[%Y-%b-%d %H:%M]')
+	LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+	return response
 # <---
 
 
