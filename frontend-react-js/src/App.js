2a3836171e09ed8f4ebe0237888693329124a386
SinnumUGesgag A <michael.shrader@icloud.com>
2025-06-14 23:19:06 +0000

    Utilizing AWS-Amplify V5

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -11,10 +11,45 @@ import MessageGroupPage from './pages/MessageGroupPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
 
+// Cognito --->
+import { Amplify, Auth } from 'aws-amplify';
+// <---
+
 import {
   createBrowserRouter,
   RouterProvider
 } from "react-router-dom";
+
+// Amplify Cognito --->
+Amplify.configure( {
+	"AWS_PROJECT_REGION": process.env.REACT_APP_AWS_PROJECT_REGION,
+	"aws_cognito_region": process.env.REACT_APP_AWS_COGNITO_REGION,
+	"aws_user_pools_id": process.env.REACT_APP_AWS_USER_POOLS_ID,
+	"aws_user_pools_web_client_id": process.env.REACT_APP_CLIENT_ID,
+	
+	Auth: {
+		// REQUIRED - Amazon Cognito Region
+		region: process.env.REACT_APP_AWS_COGNITO_REGION,
+
+		// OPTIONAL - Amazon Cognito User Pool ID
+		userPoolId: process.env.REACT_APP_AWS_USER_POOLS_ID,
+
+		// OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+		userPoolWebClientId: process.env.REACT_APP_CLIENT_ID,
+
+		// OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+		//mandatorySignIn: false,
+
+		// OPTIONAL - This is used when autoSignIn is enabled for Auth.signUp
+		// 'code' is used for Auth.confirmSignUp, 'link' is used for email link verification
+		signUpVerificationMethod: 'code', 
+	}
+});
+
+// You can get the current config object
+const currentConfig = Auth.configure();
+
+// <---
 
 const router = createBrowserRouter([
   {
