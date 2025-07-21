a841e6cde7b6ba42ebcd4145676e431641bf18b0
SinnumUGesgag A <michael.shrader@icloud.com>
2025-07-21 21:33:01 +0000

    Updating home_activities & fixing a typo

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -3,17 +3,18 @@ import os
 
 def query_wrap_object(template):
 	sql = f'''
-	{SELECT COALESCE(row_to_json(object_row),'{}'::json) FROM (
+	(SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
 	{template}
 	) object_row);
 	'''
 
 def query_wrap_array(template):
 	sql = f'''
-	{SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'{}'::json) FROM (
+	(SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'{{}}'::json) FROM (
 	{template}
 	) array_row);
 	'''
 
 connection_url = os.getenv("CONNECTION_URL")
 pool = ConnectionPool(connection_url)
+
