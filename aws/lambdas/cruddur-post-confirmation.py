b08a1fbf07f9935f1489dadee6ad79f1fae91f30
SinnumUGesgag A <michael.shrader@icloud.com>
2025-08-20 18:15:09 +0000

    Lambda Confirmation is Setup for Sign Ups

--- aws/lambdas/cruddur-post-confirmation.py
+++ aws/lambdas/cruddur-post-confirmation.py
@@ -1,8 +1,11 @@
 import json
 import psycopg2
+import os
 
 def lambda_handler(event, context):
 	user = event['request']['userAttributes']
+	print('userAttributes')
+	print(user)
 	
 	user_display_name =user['name']
 	user_handle = user['preferred_username']
@@ -10,27 +13,31 @@ def lambda_handler(event, context):
 	user_cognito_id = user['sub']
 	
 	try:
+		print('entered-try')
 		sql = f"""
-			"INSERT INTO public.users (
+			INSERT INTO public.users (
 				display_name,
 				handle,
 				email, 
 				cognito_user_id
 			) 
 			VALUES (
-				{user_display_name},
-				{user_handle},
-				{user_email},
-				{user_cognito_id}
-			)"
+				'{user_display_name}',
+				'{user_handle}',
+				'{user_email}',
+				'{user_cognito_id}'
+			)
 		"""
-		
-		conn = psycopg2.connect(os.getnev('CONNECTION_URL'))
+
+		print('----SQL----')
+		print(sql)
+
+		conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
 		cur = conn.cursor()
 		cur.execute(sql)
 		conn.commit()
 	
-	except (Exceptioon, psycopg2.DatabaseError) as error:
+	except (Exception, psycopg2.DatabaseError) as error:
 		print(error)
 	
 	finally:
