f115a284a30ce902f5a4dab494635892894c13c0
SinnumUGesgag A <michael.shrader@icloud.com>
2025-09-15 18:45:46 +0000

    Added Error Handling to db.py & dydb-seed
    Running into a lot of strange errors that
    come down to how the db.py file behaves
    and/or how it has been invoked through out
    the course ; so running some error catching
    to help me pinpoint the causes of the problems

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -8,35 +8,21 @@ from flask import current_app as app
 class InteractSQLDB:
 	def __init__(self):
 		self.init_pool()
-	
-	def template(self, *args):
-		
-		# finds the root path listing it as the first entry, then the name of the folders where
-		# I've stored the SQL Tempplates; then it lists the args as individual entries
-
-		pathing = list((app.root_path,'db','sql',) + args)
-		# adding to the very end of the list ".sql" which is the file type for the SQL files we'll  be referrencing for our SQL Templates
-		pathing[-1] = pathing[-1] + ".sql"
-
-		# Joins each individual entry within the Pathing List while then navigating that path to find the file that we're going to read
-		template_path = os.path.join(*pathing)
-		# opens the file with reading privileges only
-		with open(template_path, 'r') as f:
-			# reads the file's contents and places them as a string into the template_content variable, to be returned for use as our SQL object that we're going to pass into functions that require an SQL input
-			template_content = f.read()
-		return template_content
-
+		# print(f"""------__init__-----""")
+		# print("whatever error I am monitoring")
 
 	# intializing the Psycopg Connnectino Pool to our SQL DB
 	def init_pool(self):
 		connection_url = os.getenv("PSQL_CRUDDUER_DB_URL")
 		self.pool = ConnectionPool(connection_url)
+		print(f"""------init_pool-----""")
+		print(type(self))
 
 	# to INSERT into the SQL DB while utilizing a connection in our Psycopg Connection Pool 
 	def query_commit_returning_id(self, sql, params={}):
 
-		app.logger.info(f"query_commit_returning_id : params : {params} ----------")
-		app.logger.info(f"query_commit_returning_id : sql : {sql} ------------")
+		# app.logger.info(f"query_commit_returning_id : params : {params} ----------")
+		# app.logger.info(f"query_commit_returning_id : sql : {sql} ------------")
 
 		# make sure to check for RETURNING in all uppercases
 		pattern = r"\bRETURNING\b"
@@ -55,12 +41,17 @@ class InteractSQLDB:
 						return returning_id
 		except Exception as errors:
 			app.logger.info(f"----- errors: {errors}")
+		# print(f"""------query_commit_returning_id-----""")
+		# print("whatever error I am monitoring")
 
 	# to query an array of json objects
 	def query_json_array(self, sql, params={}):
-		app.logger.info(f"query_json_array : params : {params} ----------")
-		app.logger.info(f"query_json_array : sql : {sql} ------------")
+		#app.logger.info(f"query_json_array : params : {params} ----------")
+		#app.logger.info(f"query_json_array : sql : {sql} ------------")
 
+		# print(f"""------query_json_array-----""")
+		# print("whatever error I am monitoring")
+		
 		wrapped_sql = self.query_wrap_array(sql)	
 		with self.pool.connection() as conn:
 			with conn.cursor() as cur:
@@ -73,8 +64,8 @@ class InteractSQLDB:
 	# to query a json object from the DB
 	def query_json_object(self, sql, uuid):
 
-		app.logger.info(f"query_json_object : uuid : {uuid} ----------")
-		app.logger.info(f"query_json_object : sql : {sql} ------------")
+		# app.logger.info(f"query_json_object : uuid : {uuid} ----------")
+		# app.logger.info(f"query_json_object : sql : {sql} ------------")
 
 		wrapped_sql = self.query_wrap_object(sql)	
 		with self.pool.connection() as conn:
@@ -87,23 +78,29 @@ class InteractSQLDB:
 					"{}"
 				else:
 					return json[0]
+		# print(f"""------query_json_object-----""")
+		# print("whatever error I am monitoring")
 
 
-	def query_wrap_object(self, template):
-		sql = f'''
+	def query_wrap_object(self, sql):
+		new_sql = f'''
 		(SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
-		{template}
+		{sql}
 		) object_row);
 		'''
-		return sql
+		# print(f"""------query_wrap_object-----""")
+		# print("whatever error I am monitoring")
+		return new_sql
 
-	def query_wrap_array(self, template):
-		sql = f'''
+	def query_wrap_array(self, sql):
+		new_sql = f'''
 		(SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'{{}}'::json) FROM (
-		{template}
+		{sql}
 		) array_row);
 		'''
-		return sql
+		# print(f"""------query_wrap_array-----""")
+		# print("whatever error I am monitoring")
+		return new_sql
 
 
 	# # to INSERT into the SQL BD & have it return the UUID of the new entry 
@@ -119,7 +116,27 @@ class InteractSQLDB:
 	# 	except Exception as errors:
 	# 		app.logger.info(f"----- errors: {errors}")
 
+	def template(self, *args):
 		
+		# finds the root path listing it as the first entry, then the name of the folders where
+		# I've stored the SQL Tempplates; then it lists the args as individual entries
 
-db = InteractSQLDB()
+		pathing = list((app.root_path,'db','sql',) + args)
+		# adding to the very end of the list ".sql" which is the file type for the SQL files we'll  be referrencing for our SQL Templates
+		pathing[-1] = pathing[-1] + ".sql"
+
+		# Joins each individual entry within the Pathing List while then navigating that path to find the file that we're going to read
+		template_path = os.path.join(*pathing)
+		# opens the file with reading privileges only
+		with open(template_path, 'r') as f:
+			# reads the file's contents and places them as a string into the template_content variable, to be returned for use as our SQL object that we're going to pass into functions that require an SQL input
+			template_content = f.read()
+		
+		# print(f"""------template-----""")
+		# print("whatever error I am monitoring")
+		return template_content
+
+
+
+#db = InteractSQLDB()
 
